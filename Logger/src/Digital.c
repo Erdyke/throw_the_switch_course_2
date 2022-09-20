033c7071aa89524eae515cf426680d5a491c03cb
Eric Dyke <edyke@willowcomputer.com>
2022-09-20 07:17:59 -0400

    Completed 2.15 including Digital source and tests.

--- Logger/src/Digital.c
+++ Logger/src/Digital.c
@@ -1,1 +1,31 @@
+#include "Core.h"
 #include "Digital.h"
+#include "LPC1768.h"
+#include "Utils.h"
+
+#define ALL_DIGITAL_BITS (MASK_RANGE(0, 5))
+
+#define DIGITAL_PORT     LPC_GPIO2
+#define DIGITAL_PIN_MIN  (0)
+#define DIGITAL_PIN_MAX  (5)
+
+#define DIGITAL_PINSEL   LPC_PINCON->PINSEL4
+#define DIGITAL_PINMODE  LPC_PINCON->PINMODE4
+#define DIGITAL_PINVAL   LPC_GPIO2->FIOPIN
+
+void Digital_Init(void)
+{
+    //All our Digital I/O are going to be inputs
+    DIGITAL_PORT->FIOMASK &= ~ ALL_DIGITAL_BITS;
+    DIGITAL_PORT->FIODIR  &= ~ ALL_DIGITAL_BITS;
+
+    //Clear pin select bits for bits we plan to use to make them GPIO pins
+    DIGITAL_PINSEL  &= ~MASK_RANGE_PAIRS(DIGITAL_PIN_MIN, DIGITAL_PIN_MAX, 0x3);
+    DIGITAL_PINMODE |= MASK_RANGE_PAIRS(DIGITAL_PIN_MIN, DIGITAL_PIN_MAX, 0x3);
+}
+
+uint8_t Digital_GetBits(void)
+{
+    uint32_t pin_values = DIGITAL_PINVAL & ALL_DIGITAL_BITS;
+    return ((uint8_t) pin_values);
+}
--- Logger/test/TestDigital.c
+++ Logger/test/TestDigital.c
@@ -1,6 +1,7 @@
 #include "unity.h"
-
+#include "LPC1768.h"
 #include "Digital.h"
+#include "Utils.h"
 
 void setUp(void)
 {
@@ -12,10 +13,27 @@ void tearDown(void)
 
 void test_Digital_Init(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement Digital");
+    LPC_GPIO2->FIOMASK   = 0xFFFFFFFF;
+    LPC_GPIO2->FIODIR    = 0xFFFFFFFF;
+    LPC_PINCON->PINSEL4  = 0xFFFFFFFF;
+    LPC_PINCON->PINMODE4 = 0x00000000;
+
+    Digital_Init();
+
+    TEST_ASSERT_EQUAL_HEX(0xFFFFFFC0, LPC_GPIO2->FIOMASK);
+    TEST_ASSERT_EQUAL_HEX(0xFFFFFFC0, LPC_GPIO2->FIODIR);
+    TEST_ASSERT_EQUAL_HEX(0xFFFFF000, LPC_PINCON->PINSEL4);
+    TEST_ASSERT_EQUAL_HEX(0x00000FFF, LPC_PINCON->PINMODE4);
 }
 
 void test_Digital_GetBits_should_ReadDigitalInputsAndReturnCurrentValues(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement Digital");
+    LPC_GPIO2->FIOPIN = 0;
+    TEST_ASSERT_EQUAL_HEX(0, Digital_GetBits());
+
+    LPC_GPIO2->FIOPIN = 0x1;
+    TEST_ASSERT_EQUAL_HEX(1, Digital_GetBits());
+
+    LPC_GPIO2->FIOPIN = 0xFFFFFFFF;
+    TEST_ASSERT_EQUAL_HEX(MASK_RANGE(0, 5), Digital_GetBits());
 }
