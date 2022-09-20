3dcd6126f3629c4767a59a5c560b76b7cfd3ff7d
Eric Dyke <edyke@willowcomputer.com>
2022-09-20 06:50:40 -0400

    Implemented LED functions and tests.

--- Logger/src/LED.c
+++ Logger/src/LED.c
@@ -2,20 +2,59 @@
 #include "LPC1768.h"
 
 #include "Utils.h"
+#include "stdlib.h"
 
 #define LED_PORT LPC_GPIO1
 
-#define LED_BITS (BIT_TO_MASK(18) | \
-                  BIT_TO_MASK(20) | \
-                  BIT_TO_MASK(21) | \
-                  BIT_TO_MASK(23) )
+#define ALL_LED_BITS (BIT_TO_MASK(18) | \
+                      BIT_TO_MASK(20) | \
+                      BIT_TO_MASK(21) | \
+                      BIT_TO_MASK(23) )
+
+uint32_t led_to_mask_table[NUM_LED] =
+{
+    BIT_TO_MASK(18),
+    BIT_TO_MASK(20),
+    BIT_TO_MASK(21),
+    BIT_TO_MASK(23),
+};
 
 void LED_Init(void)
 {
     /* Configure for outputs */
 
-    LED_PORT->FIODIR = LED_BITS;
-    LED_PORT->FIOSET = LED_BITS;
+    LED_PORT->FIODIR = ALL_LED_BITS;
+    LED_PORT->FIOSET = ALL_LED_BITS;
 
     /* Set high to drive LEDs */
+}
+
+void LED_On(LED_ID_T led_id)
+{
+    if(led_id >= NUM_LED)
+        return;
+
+    LED_PORT->FIOSET |= led_to_mask_table[led_id];
+}
+
+void LED_Off(LED_ID_T led_id)
+{
+    if(led_id >= NUM_LED)
+        return;
+
+    LED_PORT->FIOCLR |= led_to_mask_table[led_id];
+}
+
+void LED_Toggle(LED_ID_T led_id)
+{
+    uint32_t pin_mask = led_to_mask_table[led_id];
+
+    if(LPC_GPIO1->FIOPIN & pin_mask)
+    {
+        LPC_GPIO1->FIOCLR |= pin_mask;
+    }
+    else
+    {
+        LPC_GPIO1->FIOSET |= pin_mask;
+    }
 }
--- Logger/test/TestLED.c
+++ Logger/test/TestLED.c
@@ -9,6 +9,21 @@
                            BIT_TO_MASK(21) | \
                            BIT_TO_MASK(23) )
 
+extern uint32_t led_to_mask_table[NUM_LED];
+
+
+void helperUpdatePin(void)
+{
+    //Apply pin state
+    LPC_GPIO1->FIOPIN |=  LPC_GPIO1->FIOSET;
+    LPC_GPIO1->FIOPIN &= ~LPC_GPIO1->FIOCLR;
+
+    //Clear set/clr registers
+    LPC_GPIO1->FIOSET = 0;
+    LPC_GPIO1->FIOCLR = 0;
+}
+
+
 void test_led_init_turns_on_only_LEDs(void)
 {
     LPC_GPIO1->FIODIR = 0;
@@ -21,17 +36,56 @@ void test_led_init_turns_on_only_LEDs(void)
 }
 
 
-void test_LED_Toggle_should_ToggleTheCorrectOutput(void)
-{
-    TEST_IGNORE_MESSAGE("Implement LED_Toggle");
-}
-
 void test_LED_On_should_EnableTheCorrectOutput(void)
 {
-    TEST_IGNORE_MESSAGE("Implement me");
+    char msg[100];
+
+    for(LED_ID_T led_id = 0; led_id < NUM_LED; led_id++)
+    {
+        helperUpdatePin();
+        LED_On(led_id);
+
+        sprintf(msg, "LED id %d", led_id);
+        TEST_ASSERT_EQUAL_HEX32_MESSAGE(led_to_mask_table[led_id], LPC_GPIO1->FIOSET, msg);
+    }
 }
 
-void test_LED_Off_should_DisableTheCorrectOutput(void)
+void test_LED_Off_should_EnableTheCorrectOutput(void)
 {
-    TEST_IGNORE_MESSAGE("Implement me");
+    char msg[100];
+
+    for(LED_ID_T led_id = 0; led_id < NUM_LED; led_id++)
+    {
+        helperUpdatePin();
+        LED_Off(led_id);
+
+        sprintf(msg, "LED id %d", led_id);
+        TEST_ASSERT_EQUAL_HEX32_MESSAGE(led_to_mask_table[led_id], LPC_GPIO1->FIOCLR, msg);
+    }
+}
+
+
+void test_LED_Toggle_should_ToggleTheCorrectOutput(void)
+{
+    char set_msg[100];
+    char clr_msg[100];
+
+    LPC_GPIO1->FIOPIN = 0;
+    LPC_GPIO1->FIOSET = 0;
+    LPC_GPIO1->FIOCLR = 0;
+
+    for(LED_ID_T led_id = 0; led_id < NUM_LED; led_id++)
+    {
+        uint32_t exp_pin_mask = led_to_mask_table[led_id];
+        sprintf(set_msg, "LED SET id %d", led_id);
+        sprintf(clr_msg, "LED CLR id %d", led_id);
+
+        LED_Toggle(led_id);
+        TEST_ASSERT_EQUAL_HEX32_MESSAGE(exp_pin_mask, LPC_GPIO1->FIOSET, set_msg);
+        helperUpdatePin();
+
+        LED_Toggle(led_id);
+        TEST_ASSERT_EQUAL_HEX32_MESSAGE(exp_pin_mask, LPC_GPIO1->FIOCLR, clr_msg);
+        helperUpdatePin();
+    }
 }
